80f94584b62dd34fd782c07ba684a50ee1828b37
Allen Liu <kamyu104@gmail.com>
2015-05-17 02:14:50 +0800

    add solution

--- ch3/Exercises/UVa11809.cpp
+++ ch3/Exercises/UVa11809.cpp
@@ -15,14 +15,14 @@ struct HashPair {
 
 int main() {
     unordered_map<pair<double, int>, pair<int,int>, HashPair> map;
-    // Build up the mapping table from "c * 10^d" to "a * 2^b"
+    // Build up the mapping table from "c * 10^d" to "a * 2^b".
     for (int i = 0; i <= 9; ++i) {
         for (int j = 1; j <= 30; ++j) {
             double a = 1 - pow(2, -(i + 1)); // max mantissa = 1 - 2^(-i-1)
             int b = pow(2, j) - 1;        // max exponent = 2^j - 1
             
             // a * 2^b = c * 10^d => log10(a) + b * log10(2) = log10(c) + d
-            // Let d = floor(log10(a) + b * log10(2)), then c = 10^(x-d)
+            // Let d = floor(log10(a) + b * log10(2)), then c = 10^(x-d).
             double x = log10(a) + b * log10(2);
             int d = floor(x);
             double c = pow(10, x - d);
@@ -38,7 +38,7 @@ int main() {
         double p;
         int q;
         ss >> p >> q;
-        // Lookup the mapping table from "c * 10^d" to "a * 2^b"
+        // Lookup the mapping table from "c * 10^d" to "a * 2^b".
         for (const auto& kv : map) {
             pair<double, int> key;
             pair<int, int> val;
