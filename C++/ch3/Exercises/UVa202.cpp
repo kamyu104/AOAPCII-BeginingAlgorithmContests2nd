a4a5331a16ef838ea5c1fb5cfd3750fd9791616e
Allen Liu <kamyu104@gmail.com>
2015-05-21 02:39:37 +0800

    update

--- C++/ch3/Exercises/UVa202.cpp
+++ C++/ch3/Exercises/UVa202.cpp
@@ -1,21 +1,34 @@
-// UVa202 - Repeating Decimals
-// Time:  O(d), d is size of the denominator
-// Space: O(d)
+// Copyright (c) 2015 kamyu. All rights reserved.
+
+/*
+ * UVa202 - Repeating Decimals
+ * http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=138
+ *
+ * Time  : O(d), d is size of the denominator
+ * Space : O(d)
+ *
+ */
 
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 
-using namespace std;
+using std::vector;
+using std::unordered_map;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::min;
 
 int main() {
     const int limit = 50;
     int a, b;
-    
+
     while (cin >> a >> b) {
         unordered_map<int, int> remainder_index;
         vector<int> decimal_digits;
-        
+
         // Count the decimals part.
         int index = 0;
         int result = a / b;
@@ -26,16 +39,16 @@ int main() {
             decimal_digits.emplace_back(result);
             remainder = (remainder * 10) % b;
         }
-        
+
         // Print out the integer part.
         cout <<  a << "/" << b << " = " << a / b << ".";
-        
+
         // Print out the decimals.
         int cycle_start_index = remainder_index[remainder], n_cur_digit = 0;
-        for (; n_cur_digit < cycle_start_index && n_cur_digit < limit; ++n_cur_digit) {
+        for (; n_cur_digit < min(cycle_start_index, limit); ++n_cur_digit) {
             cout << decimal_digits[n_cur_digit];
         }
-        
+
         // Print out repeating part of the decimals.
         cout << "(";
         for (; n_cur_digit < min(index, limit); ++n_cur_digit) {
@@ -45,11 +58,11 @@ int main() {
             cout << "...";
         }
         cout << ")" << endl;
-        
+
         // Print out number of digits in repeating cycle.
         cout << "   " << index - cycle_start_index
              << " = number of digits in repeating cycle" << endl << endl;
     }
-    
+
     return 0;
 }
