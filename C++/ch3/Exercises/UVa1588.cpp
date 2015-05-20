6e556e9e33cc9aefeefc86c9d0878f56e7800817
Allen Liu <kamyu104@gmail.com>
2015-05-21 03:23:00 +0800

    update

--- C++/ch3/Exercises/UVa1588.cpp
+++ C++/ch3/Exercises/UVa1588.cpp
@@ -1,13 +1,27 @@
-// UVa1588 - Kickdown
-// Time:  O(m * n)
-// Space: O(1)
+// Copyright (c) 2015 kamyu. All rights reserved.
+
+/*
+ * UVa1588 - Kickdown
+ * http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=4463
+ *
+ * Time  : O(m * n)
+ * Space : O(1)
+ *
+ */
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::max;
+using std::min;
 
-bool IsValid(int k, const string& s1, const string& s2){
+
+bool IsValid(const int k, const string& s1, const string& s2) {
     for (int i = 0; k + i < s1.length() && i < s2.length(); ++i) {
         if ((s1[k + i] - '0') + (s2[i] - '0') > 3) {
             return false;
@@ -16,7 +30,7 @@ bool IsValid(int k, const string& s1, const string& s2){
     return true;
 }
 
-size_t Cut(const string& s1, const string& s2){
+size_t Cut(const string& s1, const string& s2) {
     int k = 0;
     while (!IsValid(k, s1, s2)) {
         ++k;
@@ -24,7 +38,7 @@ size_t Cut(const string& s1, const string& s2){
     return max(s1.length(), s2.length() + k);
 }
 
-int main(){
+int main() {
     string bottom, top;
     while (cin >> bottom >> top) {
         cout << min(Cut(bottom, top), Cut(top, bottom)) << endl;
