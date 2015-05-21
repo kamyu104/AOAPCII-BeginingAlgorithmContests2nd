150c55bb298a5c8cd5445c9d11710eb01d9ff953
Allen Liu <kamyu104@gmail.com>
2015-05-21 10:01:26 +0800

    update

--- C++/ch3/Exercises/UVa1587.cpp
+++ C++/ch3/Exercises/UVa1587.cpp
@@ -1,13 +1,29 @@
-// UVa1587 - Box
-// Time:  O(1)
-// Space: O(1)
+// Copyright (c) 2015 kamyu. All rights reserved.
+
+/*
+ * UVa1587 - Box
+ * http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=830&page=show_problem&problem=4462
+ *
+ * Time  : O(1)
+ * Space : O(1)
+ *
+ */
 
 #include <iostream>
 #include <unordered_set>
 #include <unordered_map>
 #include <algorithm>
+#include <utility>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::unordered_set;
+using std::unordered_map;
+using std::hash;
+using std::pair;
+using std::make_pair;
+using std::swap;
 
 struct HashPair {
     size_t operator()(const pair<int, int>& p) const {
@@ -16,19 +32,25 @@ struct HashPair {
 };
 
 // Make sure all of (w, h) has pairs.
-bool CheckArea(unordered_map<pair<int, int>, int, HashPair>& area) {
-    return all_of(area.cbegin(), area.cend(), [](const pair<pair<int, int>, int>& p){ return p.second % 2 == 0; });
+bool CheckArea(const unordered_map<pair<int, int>, int, HashPair>& area) {
+    return all_of(area.cbegin(), area.cend(),
+                  [](const pair<pair<int, int>, int>& p) {
+                      return p.second % 2 == 0;
+                  });
 }
 
-// Make sure at most 3 different lengths of w, h, l, and all pairs of l, w, h exist.
-bool CheckEdge(unordered_map<pair<int, int>, int, HashPair>& area, unordered_set<int>& edge) {
+// Make sure at most 3 different lengths of w, h, l,
+// and all pairs of l, w, h exist.
+bool CheckEdge(const unordered_map<pair<int, int>, int, HashPair>& area,
+               const unordered_set<int>& edge) {
     if (edge.size() <= 3) {
         for (auto it = edge.cbegin(); it != edge.cend(); ++it) {
             auto jt = it;
             ++jt;
             for (; jt != edge.cend(); ++jt) {
+                // Some of (w, h) does not exist.
                 if (area.find(make_pair(*it, *jt)) == area.cend() &&
-                    area.find(make_pair(*jt, *it)) == area.cend()) { // Some of (w, h) does not exist.
+                    area.find(make_pair(*jt, *it)) == area.cend()) {
                     return false;
                 }
             }
@@ -38,7 +60,8 @@ bool CheckEdge(unordered_map<pair<int, int>, int, HashPair>& area, unordered_set
     return false;
 }
 
-void CheckBox(unordered_map<pair<int, int>, int, HashPair>& area, unordered_set<int>& edge) {
+void CheckBox(const unordered_map<pair<int, int>, int, HashPair>& area,
+              const unordered_set<int>& edge) {
         if (CheckArea(area) && CheckEdge(area, edge)) {
             cout << "POSSIBLE" << endl;
         } else {
@@ -51,7 +74,7 @@ int main() {
     while (cin >> w >> h) {
         unordered_map<pair<int, int>, int, HashPair> area;
         unordered_set<int> edge;
-        
+
         int count = 6;
         do {
             if (w > h) {
@@ -60,9 +83,9 @@ int main() {
             ++area[make_pair(w, h)];
             edge.insert(w), edge.insert(h);
         } while (--count && cin >> w >> h);
-        
+
         CheckBox(area, edge);
     }
-    
+
     return 0;
 }
