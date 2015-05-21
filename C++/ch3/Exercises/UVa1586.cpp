9fe8c61a41bc103263894ebc0dfbd1541499d384
Allen Liu <kamyu104@gmail.com>
2015-05-21 09:30:19 +0800

    update

--- C++/ch3/Exercises/UVa1586.cpp
+++ C++/ch3/Exercises/UVa1586.cpp
@@ -1,14 +1,25 @@
-// UVa1586 - Molar Mass
-// Time:  O(n)
-// Space: O(1)
+// Copyright (c) 2015 kamyu. All rights reserved.
+
+/*
+ * UVa1586 - Molar Mass
+ * http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=830&page=show_problem&problem=4461
+ *
+ * Time  : O(n)
+ * Space : O(1)
+ *
+ */
 
 #include <iostream>
 #include <string>
 #include <cstdlib>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::ios;
 
-double AtomicWeightOf(char atom) {
+double AtomicWeightOf(const char atom) {
     switch (atom) {
         case 'C':
             return 12.01;
@@ -28,31 +39,32 @@ double AtomicWeightOf(char atom) {
 int main() {
     int T;
     cin >> T;
-    
+
     while (T--) {
         string formula;
         cin >> formula;
-        
+
         double result = 0;
         int i = 0;
         while (i < formula.length()) {
             // Parse the number of the atom.
             string temp_str = "0";
             int temp_index = i + 1;
-            while (temp_index < formula.length() && isdigit(formula[temp_index])) {
+            while (temp_index < formula.length() &&
+                   isdigit(formula[temp_index])) {
                 temp_str.append(1, formula[temp_index]);
                 ++temp_index;
             }
-            
+
             double multiplier = atof(temp_str.c_str());
             if (multiplier == 0.0) {
                 multiplier = 1.0;
             }
             result += multiplier * AtomicWeightOf(formula[i]);
-            
+
             i = temp_index;
         }
-        cout.setf(ios::fixed,ios::floatfield);
+        cout.setf(ios::fixed, ios::floatfield);
         cout.precision(3);
         cout << result << endl;
     }
